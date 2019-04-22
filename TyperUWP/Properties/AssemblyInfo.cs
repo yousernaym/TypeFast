36f2df2ee8b4b853560e76d5b28df13bf18a2c9f
kloda <mickeh.se@hotmail.com>
2019-04-23 01:31:47 +0200

    Changed AssemblyInfo and Package manifest.

--- TyperUWP/Properties/AssemblyInfo.cs
+++ TyperUWP/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("TyperUWP")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A tool to train/test your typing skill.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Mikesoft")]
 [assembly: AssemblyProduct("TyperUWP")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
